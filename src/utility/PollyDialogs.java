b5773c15ff6e560ea73ffe17c932cbf2eae9d568
Nuno Fonseca <nunofonsecaflorencio@gmail.com>
2022-11-30 07:36:30 +0200

    Revert "Custom JDialog (Parte 1)"
    This reverts commit e19ae563bc6b3575e5aca6bc1d60a0922657d7be.

--- src/utility/PollyDialogs.java
+++ src/utility/PollyDialogs.java
@@ -5,27 +5,20 @@ import java.awt.*;
 
 public class PollyDialogs {
 
-    public PollyDialogs() {
-        //test
-        JOptionPane pane = new JOptionPane("Your Message", JOptionPane.PLAIN_MESSAGE, JOptionPane.DEFAULT_OPTION);
-        getComponents(pane);
-        pane.setBackground(PollyConstants.LIGHT);
-        
-//        JDialog p = new JDialog();
-//        p = pane.createDialog(this, "Message");
-//        p.setVisible(true);
-        
+    public static void showMessageDialog(String message){
+        // Frame = Polly.getFrame || modal = true
     }
-    
-    public static void getComponents(Container c) {
-        Component[] m = c.getComponents();
-        
-        for (int i = 0; i < m.length; i++) {
-            if ("java.swing.JPanel".equals(m[i].getClass().getName()))
-                m[i].setBackground(PollyConstants.HIGHLIGHT);
-            
-            if (c.getClass().isInstance(m[i]))
-                getComponents((Container)m[i]);   
+
+    public static void showErrorDialog(String message){
+
+    }
+
+
+
+    private class GenericDialog extends JDialog{
+
+        public GenericDialog(Frame owner, boolean modal) {
+            super(owner, modal);
         }
     }
 }
